50759193a1138146e435fd0be5f311b1ab429228
Jose Diaz-Gonzalez <myaccounts@savant.be>
2009-10-04 23:57:10 -0400

    incorporates CommentiaBehavior updated readme

--- README.markdown
+++ README.markdown
@@ -1,9 +1,9 @@
-This plugin provides an easy way to include Commentable Behavior in your application.
+This plugin provides an easy way to include Commentable Behavior in your application. It also integrates Miles Johnson's (@gearvosh) CommentiaBehavior (http://www.milesj.me/resources/script/commentia-behavior) to enable some simple moderation of each comment before posting to the database.
 
 ## Installation
 - Clone from github : in your plugin directory type `git clone git://github.com/josegonzalez/commentable-behavior.git commentable`
-- Add as a git submodule : in your plugin directory type `git submodule add git://github.com/josegonzalez/commentable-behavior.git commentable`
-- Download an archive from github and extract it in `/plugins/commentable`
+- Add as a git submodule : in the directory containing your app, type `git submodule add git://github.com/josegonzalez/commentable-behavior.git app/plugins/commentable`
+- Download an archive from github and extract it in `app/plugins/commentable`
 
 ## Usage
 1. In a model that needs slugging, add :
@@ -14,28 +14,28 @@ This plugin provides an easy way to include Commentable Behavior in your applica
 
 3. Add the following to it's controller (I'm using the Post controller as an example) :
 
-    function comment($id = null) {
-        if (!empty($this->data['Comment'])) {
-            if ($this->Post->createComment($id, $this->data)){
-                $this->Session->setFlash(__('Post was commented on', true), 'messages/success');
-                $this->redirect(array('action' => 'view', $id), 200, true);
-            } else {
-                $this->Session->setFlash(__('Post could not be commented on. Perhaps you left a field empty?', true), 'messages/error');
-            }
-        }
-    }
+	function comment($id = null) {
+		if (!empty($this->data['Comment'])) {
+			if ($this->Post->createComment($id, $this->data)){
+				$this->Session->setFlash(__('Post was commented on', true), 'messages/success');
+				$this->redirect(array('action' => 'view', $id), 200, true);
+			} else {
+				$this->Session->setFlash(__('Post could not be commented on. Perhaps you left a field empty?', true), 'messages/error');
+			}
+		}
+	}
 
 4. And add the following to the view (in my case the posts/view.ctp file) you'd like to comment on:
 
-    <h2><?php __('Post a Comment'); ?></h2>
-    <?php echo $form->create('Post', array('action' => 'comment')); ?>
-        <fieldset>
-            <legend><?php __('Add Comment');?></legend>
-            <?php
-                echo $form->input('Post.id');
-                echo $form->input('Comment.name');
-                echo $form->input('Comment.email');
-                echo $form->input('Comment.body');
-            ?>
-        </fieldset>
-    <?php echo $form->end('Submit'); ?>
+	<h2><?php __('Post a Comment'); ?></h2>
+		<?php echo $form->create('Post', array('action' => 'comment')); ?>
+		<fieldset>
+			<legend><?php __('Add Comment');?></legend>
+			<?php
+				echo $form->input('Post.id');
+				echo $form->input('Comment.name');
+				echo $form->input('Comment.email');
+				echo $form->input('Comment.body');
+			?>
+		</fieldset>
+	<?php echo $form->end('Submit'); ?>
--- models/behaviors/commentable.php
+++ models/behaviors/commentable.php
@@ -3,39 +3,70 @@
  * Commentable Model Behavior
  * 
  * Allows you to attach a comment to any model in your application
- *
- * @package app.models.behaviors
- * @author Jose Diaz-Gonzalez
- * @version 0.2
- * @copyright Stoop Dev
+ * Moderates/Validates comments to check for spam.
+ * Validates comments based on a point system.High points is an automatic approval,
+ * where as low points is marked as spam or deleted.
+ * Based on Jonathan Snooks outline.
+ * 
+ * @filesource
+ * @copyright     Stoop Dev
+ * @link          http://github.com/josegonzalez/cakephp-commentable-behavior
+ * @package       commentable
+ * @subpackage    commentable.models.behaviors
+ * @version       0.3
+ * @modifiedby    Jose Diaz-Gonzalez
+ * @lastmodified  October 4th, 2009
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  **/
 class CommentableBehavior extends ModelBehavior {
 
-	/**
-	 * Contain settings indexed by model name.
-	 *
-	 * @var array
-	 * @access private
-	 */
+/**
+ * Settings initialized with the behavior
+ * @access public
+ * @var array 
+ */
+	$defaults = array( 
+		'class'				=> 'Comment',	// name of Comment model
+		'foreign_key'		=> 'foreign_id',// foreign key of Comment model
+		'counter_cache'		=> true,
+		'dependent'			=> true,		// model dependency
+		'conditions'		=> array(),		// conditions for find method on Comment model
+		'auto_bind'			=> true,		// automatically bind the model to the User model (default true),
+		'sanitize'			=> true			// whether to sanitize incoming comments
+		'column_author'		=> 'name',		// Column name for the authors name
+		'column_content'	=> 'body',		// Column name for the comments body
+		'column_email'		=> 'email',		// Column name for the authors email
+		'column_website'	=> 'website',	// Column name for the authors website 
+		'column_foreign_id'	=> 'foreign_id',// Column name of the foreign id that links to the article/entry/etc
+		'column_status'		=> 'status',	// Column name for automatic rating
+		'column_rating'		=> 'rating',	// Column name for automatic rating
+		'column_points'		=> 'points',	// Column name for accrued points
+		'blacklist_keywords'=> array('levitra', 'viagra', 'casino', 'sex', 'loan', 'finance',
+									'slots', 'debt', 'free', 'interesting', 'sorry', 'cool'),
+											// List of blacklisted words within text blocks
+		'blacklist_words'	=> array('.html', '.info', '?', '&', '.de', '.pl', '.cn'),
+											// List of blacklisted words within URLs
+		'deletion'			=> -10			// How many points till the comment is deleted (negative)
+	);
+
+/**
+ * Contain settings indexed by model name.
+ *
+ * @var array
+ * @access private
+ */
 	var $__settings = array();
 	
-	/**
-	 * Initiate behaviour for the model using settings.
-	 *
-	 * @param object $Model Model using the behaviour
-	 * @param array $settings Settings to override for model.
-	 * @access public
-	 */
+/**
+ * Initiate behaviour for the model using settings.
+ *
+ * @param object $Model Model using the behaviour
+ * @param array $settings Settings to override for model.
+ * @access public
+ */
 	function setup(&$model, $settings = array()) {
-		$default = array(
-			'class' => 'Comment',    // name of Comment model
-			'foreign_key' => 'foreign_id',    // foreign key of Comment model
-			'counter_cache' => true,
-			'dependent' => true,    // model dependency
-			'conditions' => array('Comment.class' => $model->alias),    // conditions for find method on Comment model
-			'auto_bind' => true,     // automatically bind the model to the User model (default true),
-			'sanitize' => true     // whether to sanitize incoming comments
-		);
+		$default = $this->defaults;
+		$default['conditions'] = array('Comment.class' => $model->alias);
 
 		if (!isset($this->__settings[$model->alias])) {
 			$this->__settings[$model->alias] = $default;
@@ -46,7 +77,7 @@ class CommentableBehavior extends ModelBehavior {
 		//handles model binding to the model
 		//according to the auto_bind settings (default true)
 		if ($this->__settings[$model->alias]['auto_bind']) {
-			$commonHasMany = array(
+			$hasManyComment = array(
 				'Comment' => array(
 					'className' => $this->__settings[$model->alias]['class'],
 					'foreignKey' => $this->__settings[$model->alias]['foreign_key'],
@@ -59,28 +90,36 @@ class CommentableBehavior extends ModelBehavior {
 					'counterCache' => $this->__settings[$model->alias]['counter_cache']
 					)
 				);
-			$model->bindModel(array('hasMany' => $commonHasMany), false);
+			$model->bindModel(array('hasMany' => $hasManyComment), false);
 			$model->Comment->bindModel(array('belongsTo' => $commentBelongsTo), false);
 		}
 	}
 
-	function createComment(&$model, $commentData = array()){
-		if (!empty($commentData['Comment']) ) {
-			$commentData['Comment']['class'] = $model->alias;
-			$commentData['Comment']['foreign_id'] = $commentData[$model->alias]['id'];
+	function createComment(&$model, $data = array()){
+		if (!empty($data['Comment']) ) {
+			$data['Comment']['class'] = $model->alias;
+			$data['Comment'][$this->__settings[$model->alias]['column_foreign_id']] = $commentData[$model->alias]['id'];
+			$data['Comment'][$this->__settings[$model->alias]['column_rating']] = $this->_rateComment($model, $data['Comment']);
+			$data['Comment'][$this->__settings[$model->alias]['column_status']] = 'approved';
 			if ($this->__settings[$model->alias]['sanitize']) {
 				App::import('Sanitize');
-				$commentData['Comment']['name'] = Sanitize::clean($commentData['Comment']['name']);
-				$commentData['Comment']['email'] = Sanitize::clean($commentData['Comment']['email']);
-				$commentData['Comment']['body'] = Sanitize::clean($commentData['Comment']['body']);
+				$data['Comment'][$this->__settings[$model->alias]['column_author']] =
+					Sanitize::clean($data['Comment'][$this->__settings[$model->alias]['column_author']]);
+				$data['Comment'][$this->__settings[$model->alias]['column_email']] =
+					Sanitize::clean($data['Comment'][$this->__settings[$model->alias]['column_email']]);
+				$data['Comment'][$this->__settings[$model->alias]['column_content']] =
+					Sanitize::clean($data['Comment'][$this->__settings[$model->alias]['column_content']]);
 			} else {
-				$commentData['Comment']['name'] = $commentData['Comment']['name'];
-				$commentData['Comment']['email'] = $commentData['Comment']['email'];
-				$commentData['Comment']['body'] = $commentData['Comment']['body'];
+				$data['Comment'][$this->__settings[$model->alias]['column_author']] =
+					$data['Comment'][$this->__settings[$model->alias]['column_author']];
+				$data['Comment'][$this->__settings[$model->alias]['column_email']] =
+					$data['Comment'][$this->__settings[$model->alias]['column_email']];
+				$data['Comment'][$this->__settings[$model->alias]['column_content']] =
+					$data['Comment'][$this->__settings[$model->alias]['column_content']];
 			}
-			if ($this->__checkForEmptyVal($commentData['Comment']) == false) {
+			if ($this->_checkForEmptyVal($data['Comment']) == false) {
 				$model->Comment->create();
-				if ($model->Comment->save($commentData)) {
+				if ($model->Comment->save($data)) {
 					return true;
 				}
 			}
@@ -88,36 +127,177 @@ class CommentableBehavior extends ModelBehavior {
 		return false;
 	}
 
-	function getComments(&$model, $parameters = array()){
-		$parameters = array_merge(array('id' => $model->id, 'options' => array()), $parameters);
-		$options = array();
-		if (isset($parameters['id']) && is_numeric($parameters['id'])) {
-			$options = array_merge_recursive(
-				array('conditions' => array("{$this->__settings[$model->alias]['class']}.{$this->__settings[$model->alias]['foreign_key']}" => $parameters['id'])),
-				$parameters['options']
+	function getComments(&$model, $options = array()){
+		$options = array_merge(array('id' => $model->id, 'options' => array()), $options);
+		$parameters = array();
+		if (isset($options['id']) && is_numeric($options['id'])) {
+			$parameters = array_merge_recursive(
+				array('conditions' =>
+					array(
+						$this->__settings[$model->alias]['class'] . $this->__settings[$model->alias]['foreign_key'] => $options['id'],
+						$this->__settings[$model->alias]['class'] . $this->__settings[$model->alias]['column_status'] => 'status')),
+				$options['options']
 			);
 		}
-		if (isset($options) && !$this->__checkForEmptyVal($options)){
-			return $model->Comment->find('all',
-				$options
-			);
-		} else {
-			return $model->Comment->find('all');
+		$parameters = (isset($parameters) && !$this->_checkForEmptyVal($parameters)) ? $parameters : array();
+		return $model->Comment->find('all', $parameters);
+	}
+
+	function _rateComment($model, $data) {
+		if (!empty($data)) {
+			$points += $this->_rateLinks($model, $data);
+			$points += $this->_rateLength($model, $data);
+			$points += $this->_rateEmail($model, $data);
+			$points += $this->_rateKeywords($model, $data);
+			$points += $this->_rateStartingWord($model, $data);
+			$points += $this->_rateAuthorName($model, $data);
+			$points += $this->_rateByPreviousComment($model, $data);
+			$points += $this->_rateBody($model, $data);
+			if ($points >= 1) {
+				return 'approved';
+			} else if ($points == 0) {
+				return 'pending';
+			} else if ($points <= $this->__settings[$model->alias]['deletion']) {
+				return 'delete';
+			}
+			return 'spam';
+		}
+		return 'delete';
+	}
+
+	function _rateLinks($model, $data) {
+		$links = preg_match_all(
+			"#(^|[\n ])(?:(?:http|ftp|irc)s?:\/\/|www.)(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,4}(?:[-a-zA-Z0-9._\/&=+%?;\#]+)#is", 
+			$data[$this->__settings[$model->alias]['column_content']], $matches);
+		$links = $matches[0];
+		
+		$totalLinks = count($links);
+		$length = mb_strlen($data[$this->__settings[$model->alias]['column_content']]);
+
+		// How many links are in the body
+		// -1 per link if over 2, otherwise +2 if less than 2
+		$points = ($totalLinks > 2) ? $totalLinks * -1 : 2;
+		
+		// URLs that have certain words or characters in them
+		// -1 per blacklisted word
+		// URL length
+		// -1 if more then 30 chars
+		foreach ($links as $link) {
+			foreach ($this->__settings[$model->alias]['blacklist_words'] as $word) {
+				$points = (stripos($link, $word) !== false) ? $points - 1 : $points;
+			}
+
+			foreach ($this->__settings[$model->alias]['blacklist_keywords'] as $keyword) {
+				$points = (stripos($link, $keyword) !== false) ? $points - 1 : $points;
+			}
+
+			$points = (strlen($link) >= 30) ? $points - 1 : $points;
+		}
+		return $points;
+	}
+
+	function _rateLength($model, $data) {
+		// How long is the body
+		// +2 if more then 20 chars and no links, -1 if less then 20
+		if ($length >= 20 && $totalLinks <= 0) {
+			return 2;
+		} elseif ($length >= 20 && $totalLinks == 1) {
+			return 1;
+		} elseif ($length < 20) {
+			return -1;
 		}
 	}
 
-	function __checkForEmptyVal($array) {
+	function _rateEmail($model, $data) {
+		$points = 0;
+		// Number of previous comments from email
+		// +1 per approved, -1 per spam
+		$comments = $model->Comment->find('all', array(
+			'fields' => array('Comment.id', 'Comment.status'),
+			'conditions' => array(
+				'Comment.' . $this->__settings[$model->alias]['column_email'] => $data[$this->__settings[$model->alias]['column_email']]),
+			'recursive' => -1,
+			'contain' => false
+		));
+		
+		if (!empty($comments)) {
+			foreach ($comments as $comment) {
+				if ($comment['Comment']['status'] == 'spam') {
+					--$points;
+				} elseif ($comment['Comment']['status'] == 'approved') {
+					++$points;
+				}
+			}
+		}
+		return $points;
+	}
+
+	function _rateKeywords($model, $data) {
+		$points = 0;
+		// Keyword search
+		// -1 per blacklisted keyword
+		foreach ($this->blacklistKeywords as $keyword) {
+			if (stripos($data[$this->__settings[$model->alias]['column_content']], $keyword) !== false) {
+				--$points;
+			}
+		}
+		return $points;
+	}
+
+	function _rateStartingWord($model, $data) {
+		// Body starts with...
+		// -10 points
+		$firstWord = mb_substr(
+			$data[$this->__settings[$model->alias]['column_content']],
+			0,
+			stripos($data[$this->__settings[$model->alias]['column_content']], ' '));
+
+		return (in_array(mb_strtolower($firstWord), $this->__settings[$model->alias]['blacklist_keywords'])) ? -10 : 0;
+	}
+
+	function _rateAuthorName($model, $data) {
+		// Author name has http:// in it
+		// -2 points
+		if (stripos($data[$this->__settings[$model->alias]['column_author']], 'http://') !== false) {
+			return -2;
+		}
+		return 0;
+	}
+
+	function _rateByPreviousComment($model, $data) {
+		// Body used in previous comment
+		// -1 per exact comment
+		$previousComments = $model->Comment->find('count', array(
+			'conditions' => array(
+				'Comment.' . $this->__settings[$model->alias]['column_content'] => $data[$this->__settings[$model->alias]['column_content']]),
+			'recursive' => -1,
+			'contain' => false
+		));
+		
+		return ($previousComments > 0) ? -$previousComments : return 0;
+	}
+
+	function _rateBody($model, $data) {
+		// Random character match
+		// -1 point per 5 consecutive consonants
+		$consonants = preg_match_all(
+			'/[^aAeEiIoOuU\s]{5,}+/i',
+			$data[$this->__settings[$model->alias]['column_content']],
+			$matches);
+		$totalConsonants = count($matches[0]);
+		
+		return ($totalConsonants > 0) ? -$totalConsonants : return 0;
+	}
+
+	function _checkForEmptyVal($array) {
 		$isEmpty = 0;
-		foreach($array as $key => $item) {
-			if(is_numeric($item)) {
-			} elseif(empty($item)) {
+		foreach ($array as $key => $item) {
+			if (is_numeric($item)) {
+			} elseif (empty($item)) {
 				$isEmpty++;
 			}
 		}
-		if($isEmpty > 0) {
-			return true;
-		}
-		return false;
+		return ($isEmpty > 0) ? true : false;
 	}
 }
 ?>
